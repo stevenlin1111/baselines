05b05f404972ec7f528c224091e42697da7f5fcc
Murtaza Dalal <mdalal@berkeley.edu>
2020-04-16 17:38:53 -0700

    added gmm

--- baselines/her/her.py
+++ baselines/her/her.py
@@ -97,6 +97,7 @@ def learn(*, network, env, total_timesteps,
     buffer_goals=False,
     do_skew_fit=False,
     sample_skewed_goals_from_buffer=True,
+    use_gmm=False,
     alpha=0,
     **kwargs
 ):
@@ -172,7 +173,7 @@ def learn(*, network, env, total_timesteps,
         replay_buffer=policy.buffer
     else:
         replay_buffer=None
-    rollout_worker = RolloutWorker(env, policy, dims, logger, monitor=True, replay_buffer=replay_buffer, do_skew_fit=do_skew_fit, sample_skewed_goals_from_buffer=sample_skewed_goals_from_buffer, alpha=alpha, **rollout_params)
+    rollout_worker = RolloutWorker(env, policy, dims, logger, monitor=True, replay_buffer=replay_buffer, do_skew_fit=do_skew_fit, use_gmm=use_gmm, sample_skewed_goals_from_buffer=sample_skewed_goals_from_buffer, alpha=alpha, **rollout_params)
     evaluator = RolloutWorker(eval_env, policy, dims, logger, **eval_params)
 
     n_cycles = params['n_cycles']
@@ -196,6 +197,7 @@ def learn(*, network, env, total_timesteps,
 @click.option('--buffer_goals', type=bool, default=False)
 @click.option('--do_skew_fit', type=bool, default=False)
 @click.option('--sample_skewed_goals_from_buffer', type=bool, default=True)
+@click.option('--use_gmm', type=bool, default=False)
 @click.option('--alpha', type=int, default=0)
 def main(**kwargs):
     learn(**kwargs)
